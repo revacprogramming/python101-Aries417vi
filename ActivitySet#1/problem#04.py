dfd7fc958453f62fda5ab595a3df9201e3e78c65
aries <94883201+Aries417vi@users.noreply.github.com>
2022-05-17 10:28:09 +0000

    04

--- ActivitySet#1/problem#04.py
+++ ActivitySet#1/problem#04.py
@@ -1,3 +1,10 @@
-# Conditional Execution
 
-hrs = input("Enter hours? ")
+hrs = input("Enter Hours:")
+h = float(hrs)
+a = input("Enter the Rate:")
+b = float(a)
+
+if h > 40:
+	print(40* b + (h-40)*1.5*b)
+else:
+   	print( h  * b)
--- ActivitySet#1/problem#06.py
+++ ActivitySet#1/problem#06.py
@@ -2,15 +2,17 @@
 
 largest = None
 smallest = None
-
+b=[]
 while True:
-    num = input("Enter a number? ")
-
+    num = input("Enter a number: ")
     if num == "done":
         break
-
-    # ...
-
-    print(num)
-
+    try:
+        num=int(num)
+        b.append(num)
+    except:    
+        print("Invalid input")
+largest=max(b)
+smallest=min(b)
 print("Maximum", largest)
+print("Minimum",smallest)
